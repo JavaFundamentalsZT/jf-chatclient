8a0aa2bce1d437709c592dad85e5daca130fa374
Neeme Praks <neeme.praks@zeroturnaround.com>
2014-09-17 18:15:35 +0300

    removed unused import

--- src/main/java/ee/tut/fundamentals/chat/ChatClientFx.java
+++ src/main/java/ee/tut/fundamentals/chat/ChatClientFx.java
@@ -4,15 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 
 /**
  * Simple JavaFX based chat client
